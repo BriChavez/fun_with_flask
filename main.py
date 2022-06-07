37051398aba795324dd3d18864d4f3a0fe221814
BriChavez <brianachavez@gmail.com>
2022-06-07 10:55:35 -0700

    add another function to attach to a differemnt page

--- main.py
+++ main.py
@@ -4,16 +4,29 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def index():
-    return "you did it!"
-    """    # this function defines what should be executed if the 
-            defined URL endpoint is requested by a user""" 
-            #    the naming of index is only a convention
+# def index():
+#         print("you did it!")
+#         """    # this function defines what should be executed if the 
+#             defined URL endpoint is requested by a user""" 
+#             #    the naming of index is only a convention
     
+
+def fahrenheit_from(celsius):
+    """Convert Celsius to Fahrenheit degrees."""
+    try:
+        fahrenheit = float(celsius) * 9 / 5 + 32
+        fahrenheit = round(fahrenheit, 3)  # Round to three decimal places
+        return str(fahrenheit)
+    except ValueError:
+        return "invalid input"
 
 
 if __name__ == "__main__":
-        app.run(host="127.0.0.1", port=8080, debug=True)
+    celsius = input("Celsius: ")
+    print("Fahrenheit:", fahrenheit_from(celsius))
+
+# if __name__ == "__main__":
+#         app.run(host="127.0.0.1", port=8080, debug=True)
 
 """        These two lines tell Python to start Flask’s development 
         server when the script is executed from the command line. 
